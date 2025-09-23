1c79d342e3453b708e14bb65f2b752eed985fe1c
Nguyễn Trọng Tài <ttai132002@gmail.com>
2025-09-23 09:17:45 +0700

    add router at hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import ImgHero from '../assets/Hero1.png'
 const Hero = () => {
+    const navigate = useNavigate()
     return (
         <div className='h-[85vh]  bg-green-600'>
             <div className='max-w-7xl mx-auto flex justify-between items-center h-full px-5 md:px-0'>
@@ -9,8 +11,8 @@ const Hero = () => {
                     <h1 className='text-5xl font-bold text-white'> Fresh <span className='text-orange-300'>Groceries</span> Delivered to Your Door</h1>
                     <p className='text-lg text-white mt-4'>Shop from our wide selection of fresh fruits, vegetables, dairy, and more. Get same-day delivery!</p>
                     <div className='mt-8 gap-4 flex'>
-                        <button className='bg-white text-green-600 px-6 py-2 rounded-full mr-4 cursor-pointer hover:opacity-90'>Shop Now</button>
-                        <button className='bg-green-800 border-2 border-white text-white px-6 py-2 rounded-full cursor-pointer hover:bg-green-600'>Learn More</button>
+                        <button onClick={()=> navigate('/shop')} className='bg-white text-green-600 px-6 py-2 rounded-full mr-4 cursor-pointer hover:opacity-90'>Shop Now</button>
+                        <button onClick={()=> navigate('/about')} className='bg-green-800 border-2 border-white text-white px-6 py-2 rounded-full cursor-pointer hover:bg-green-600'>Learn More</button>
                     </div>
                 </div>
                 {/* image hero */}
